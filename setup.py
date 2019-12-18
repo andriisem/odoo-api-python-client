7172f32d7a8560c02806faac8b6fef938cd6ebf9
Andrey Semko <semko.andrey.i@gmail.com>
2019-12-18 16:16:21 +0200

    version 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='odooapiclient',
-      version='0.3',
+      version='0.4',
       description='Odoo API Client',
       classifiers=[
           "Programming Language :: Python",
